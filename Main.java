d3ae8c0c8017c5ca9b2d3764d7aee8da1f6c126c
justpat99 <p.sukprapaipat@gmail.com>
2025-04-10 21:21:14 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class NotSoGoodSortz{
+public static void main(String[] args){
+int[] oOoOo = {5,2,9,1,5,6};
+    sortDem(oOoOo);
+    for(int x=0;x<oOoOo.length;x++)System.out.print(oOoOo[x]+" ");
+}
+public static void sortDem(int[] blip){
+for(int i=0;i<=blip.length;i++){
+for(int j=0;j<blip.length-1;j++){
+if(blip[j]<blip[j+1]){ // supposed to be ascending
+int tempz=blip[j];
+blip[j]=blip[j+1];
+blip[j+1]=tempz;
+}}}}
+}
